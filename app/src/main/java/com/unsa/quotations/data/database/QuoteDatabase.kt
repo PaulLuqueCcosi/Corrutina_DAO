4e6841ec115d3fa9b83927e7e427cd06f8df1a61
PoolLuque <pluquec@unsa.edu.pe>
2023-12-21 20:34:16 -0500

    exportSchema = true

--- app/src/main/java/com/unsa/quotations/data/database/QuoteDatabase.kt
+++ app/src/main/java/com/unsa/quotations/data/database/QuoteDatabase.kt
@@ -5,8 +5,8 @@ import androidx.room.RoomDatabase
 import com.unsa.quotations.data.database.daos.QuoteDao
 import com.unsa.quotations.data.database.entities.QuoteEntity
 
-//@Database(entities = [QuoteEntity::class], version = 1, exportSchema = true)
-@Database(entities = [QuoteEntity::class], version = 1)
+@Database(entities = [QuoteEntity::class], version = 1, exportSchema = true)
+//@Database(entities = [QuoteEntity::class], version = 1)
 abstract class QuoteDatabase : RoomDatabase() {
     abstract fun getQuoteDao(): QuoteDao
 }
