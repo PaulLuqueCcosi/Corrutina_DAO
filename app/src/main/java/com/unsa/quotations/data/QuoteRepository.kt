be62b0a4d1bc38750eafdaa5ab25bc7a25f91a32
PoolLuque <pluquec@unsa.edu.pe>
2023-12-21 20:24:07 -0500

    agregando Log

--- app/src/main/java/com/unsa/quotations/data/QuoteRepository.kt
+++ app/src/main/java/com/unsa/quotations/data/QuoteRepository.kt
@@ -1,10 +1,13 @@
 package com.unsa.quotations.data
 
+import android.util.Log
 import com.unsa.quotations.data.database.daos.QuoteDao
 import com.unsa.quotations.data.database.entities.QuoteEntity
 import com.unsa.quotations.data.network.QuoteService
 import com.unsa.quotations.domain.model.Quote
 import com.unsa.quotations.domain.model.toDomain
+import kotlinx.coroutines.GlobalScope.coroutineContext
+import kotlinx.coroutines.Job
 import org.jetbrains.annotations.NotNull
 import javax.inject.Inject
 
@@ -17,6 +20,7 @@ class QuoteRepository @Inject constructor (
         return response.map { it.toDomain() }
     }
     suspend fun getAllQuotesFromDatabase(): List<Quote> {
+        Log.d("corrutina","Estoy en la coroutine random-getAllQutes: ${coroutineContext[Job]}")
         val response = quoteDao.getAllQuotes()
         return response.map { it.toDomain() }
     }
